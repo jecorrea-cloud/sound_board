75c68108f0a8a78042af87036705861a51d63045
joaquin <jecorrea@protonmail.com>
2021-12-06 22:47:05 -0500

    sound stopped

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
 sounds.forEach((sound) => {
   //For each sound create the button element
   const btn = document.createElement("button");
-
   //Add a class and a text that matches with the sounds' name
   btn.classList.add("btn");
-  btn.innerText = sound;
 
+  btn.innerText = sound;
+  //Add an event to the button to play the sound
+  btn.addEventListener("click", () => {
+    //Function to make them not sound over each other
+    stopSongs();
+    document.getElementById(sound).play();
+  });
   //Grab the container and append each sound to it
   document.getElementById("buttons").appendChild(btn);
 });
+
+function stopSongs() {
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound);
+
+    song.pause();
+    song.currentTime = 0;
+  });
+}
